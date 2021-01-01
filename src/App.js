6d930ed00857ae119972ce9e7a9647487d504152
kacemws <berras.belkacem@gmail.com>
2021-01-01 15:30:51 +0100

    crafted the boilerplate for my next threejs projects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//Styles
+import "./Assets/Style/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>Boilerplate for react three fiber</h3>
+      <p>
+        Show love on
+        <a href="https://www.instagram.com/belkacember">@belkacember</a>
+      </p>
     </div>
   );
 }
