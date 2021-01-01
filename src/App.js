b5912d95420a5d96785ab1f0dc83765958ad35ed
kacemws <berras.belkacem@gmail.com>
2021-01-01 23:47:40 +0100

    necessary config, displaying the chest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-//Styles
+import { Canvas, useFrame } from "react-three-fiber";
+
+import { softShadows, Loader } from "drei";
+import Chest from "./Components/Three/Chest";
+
 import "./Assets/Style/App.scss";
+import { Suspense, useState } from "react";
+import Lights from "./Components/Three/Lights";
+import Floor from "./Components/Three/Floor";
+
+softShadows();
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="container">
-      <h3>Boilerplate for react three fiber</h3>
-      <p>
-        Show love on
-        <a href="https://www.instagram.com/belkacember">@belkacember</a>
-      </p>
-    </div>
+    <>
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{
+          position: [-5, 4, 8],
+          fov: 40,
+        }}
+      >
+        <Lights />
+        <Suspense fallback={null}>
+          <Chest open={open} setOpen={setOpen} />
+          <Floor />
+        </Suspense>
+
+        {/* Allows us to move the canvas around for different prespectives */}
+      </Canvas>
+      <Loader />
+    </>
   );
 }
 
