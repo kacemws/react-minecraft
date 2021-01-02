4770e8826363e75bf4a29600b0cddf387e8a8daa
kacemws <berras.belkacem@gmail.com>
2021-01-02 16:19:26 +0100

    defining how an item would look like

---
+++ src/Components/Item.js
@@ -0,0 +1,34 @@
+// Component: Item
+// Def: This component allows us to drag the items around & returns the actual item inside our smart blocks.
+
+import React from "react";
+import { useDrag, DragPreviewImage } from "react-dnd";
+
+const Item = ({ item, position, inventoryType }) => {
+  // The drag hook via dnd
+  const [{ isDragging }, drag, connectDragPreview] = useDrag({
+    item: { type: "item", id: item.item, position, inventoryType },
+    // Monitor if we are dragging the item
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+  // Varible which fetches the correct item from our assets
+  const itemVisual = require(`../Assets/Items/${item.item}.svg`);
+  console.log(itemVisual);
+  return (
+    <>
+      <DragPreviewImage connect={connectDragPreview} src={itemVisual.default} />
+      <div
+        ref={drag}
+        style={{
+          opacity: isDragging ? 0 : 1,
+        }}
+      >
+        <img src={itemVisual.default} alt="alt" />
+      </div>
+    </>
+  );
+};
+
+export default Item;
